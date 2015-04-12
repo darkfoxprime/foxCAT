bc3febf5e86aa0e0f8df400455f2ab00a184d864
Johnson Earls <darkfoxprime@gmail.com>
2015-04-11 18:48:52 -0700

    remove _future_ directory

--- src/_future_/t.py
+++
@@ -1,8 +0,0 @@
-
-from foxLanguagesPython import *
-
-# generate the foxLanguages mapping of language name to language class
-
-import sys
-foxLanguages = dict(tuple([(langClass.language, langClass) for langClass in [eval(langClassName) for langClassName in dir(sys.modules[__name__]) if langClassName.startswith('foxLanguages')] if issubclass(langClass, foxLanguagesBase) and langClass.language is not None]))
-
